575a5fbd38923dcf9f9f292e8371eca05b006685
mac <mac@yan.local>
2020-08-21 18:13:40 +0800

    update

--- test/user/movie/routes/detail/routes/comment/detail.test.js
+++ test/user/movie/routes/detail/routes/comment/detail.test.js
@@ -9,6 +9,34 @@ describe(`${COMMON_API} test`, function() {
 
   describe(`get the comment detail without self info test -> ${COMMON_API}`, function() {
     
+    describe(`get the comment detail without self info success test -> ${COMMON_API}`, function() {
+
+      it(`get the comment detail without self info success`, function() {
+
+      })
+
+      it(`get the comment detail without self info success but the sub comment list's length is 0`, function() {
+
+      })
+
+    })
+
+    describe(`get the comment detail without self info fail test -> ${COMMON_API}`, function() {
+
+      it(`get the comment detail without self info fail because the comment id is not found`, function() {
+
+      })
+
+      it(`get the comment detail without self info fail because the comment id is not verify`, function() {
+
+      })
+
+      it(`get the comment detail without self info fail because lack the params of comment id`, function() {
+        
+      })
+
+    })
+
   })
 
 })
--- test/user/movie/routes/detail/routes/comment/index.test.js
+++ test/user/movie/routes/detail/routes/comment/index.test.js
@@ -9,6 +9,32 @@ describe(`${COMMON_API} test`, function() {
 
   describe(`get the comment list in movie page test -> ${COMMON_API}`, function() {
     
+    describe(`get the comment list in movie page success test -> ${COMMON_API}`, function() {
+
+      it(`get the comment list in movie page success`, function() {
+
+      })
+
+      it(`get the comment list in movie page success but the list's length is 0`)
+
+    })
+
+    describe(`get the comment list in movie page fail test -> ${COMMON_API}`, function() {
+      
+      it(`get the comment list in movie page fail because the movie id is not found`, function() {
+
+      })
+
+      it(`get the comment list in movie page fail because the movie id is not verify`, function() {
+
+      })
+
+      it(`get the comment list in movie page info fail because lack of the movie id`, function() {
+        
+      })
+
+    })
+
   })
 
 })
