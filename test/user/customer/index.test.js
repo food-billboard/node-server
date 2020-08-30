ae00382366a5395d870c882475cbb7775ad931eb
gelingfeng <18368003190@163.com>
2020-08-30 11:06:02 +0800

    update

--- test/user/customer/index.test.js
+++ test/user/customer/index.test.js
@@ -10,16 +10,13 @@ const COMMON_API = '/api/user/customer'
 function responseExpect(res, validate=[]) {
 
   const { target } = res
-  // "attentions": 0,
-  // "avatar": "string",
-  // "createdAt": 1234567890,
-  // "updatedAt": 123456789,
-  // "fans": 0,
-  // "hot": 0,
-  // "username": "string",
-  // "_id": "string",
   expect(target).to.be.a('object').and.include.all.keys('attentions', 'avatar', 'fans', 'hot', 'username', '_id')
-  expect(target).to.have.property('attentions')
+  expect(target).to.have.property('attentions').and.is.a('number').and.that.above(0)
+  expect(target).to.have.property('avatar').and.is.a('string')
+  expect(target).to.have.property('fans').and.is.a('number').and.that.above(0)
+  expect(target).to.have.property('username').and.is.a('string')
+  expect(target).to.have.property('_id').and.is.a('string')
+  
 
   if(Array.isArray(validate)) {
     validate.forEach(valid => {
@@ -83,8 +80,36 @@ describe(`${COMMON_API} test`, function() {
 
     describe(`get another userinfo and not self and without self info fail test -> ${COMMON_API}`, function() {
       
-      it(`get another userinfo and not self and without self info fail because of the movie id is not found or verify`, function() {
+      it(`get another userinfo and not self and without self info fail because of the movie id is not verify`, function() {
         
+        const id = result._id.toString()
+
+        Request
+        .get(COMMON_API)
+        .query({ _id: id.slice(1) })
+        .set('Accept', 'application/json')
+        .expect(400)
+        .expect('Content-Type', /json/)
+        .end(function() {
+          done()
+        })
+
+      })
+
+      it(`get another userinfo and not self and without self info fail because of the movie id is not found`, function() {
+        
+        const id = result._id.toString()
+
+        Request
+        .get(COMMON_API)
+        .query({ _id: `${parseInt(id.slice(0, 1) + 1) % 10}${id.slice(1)}` })
+        .set('Accept', 'application/json')
+        .expect(404)
+        .expect('Content-Type', /json/)
+        .end(function() {
+          done()
+        })
+
       })
 
     })
