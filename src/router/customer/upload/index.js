720c6d3614e589e85fdd53766932aee434655a83
gelingfeng <18368003190@163.com>
2021-01-24 19:20:52 +0800

    fix bug

--- src/router/customer/upload/index.js
+++ src/router/customer/upload/index.js
@@ -293,9 +293,6 @@ router
   data = data
   .then(query => {
     const [ [ type, id ] ] = Object.entries(query)
-<<<<<<< HEAD
-    if(!ObjectId.isValid(id)) return Promise.reject({ status: 400, errMsg: 'bad request' })
-=======
     let params
     if(ObjectId.isValid(id)) {
       params = {
@@ -308,7 +305,6 @@ router
     }else {
       return Promise.reject({ status: 400, errMsg: 'bad request' })
     }
->>>>>>> 1cce330de87bd05104f25857481ebeaffaf93fcf
     
     //文件查找
     return Promise.allSettled(models.map(model => {
