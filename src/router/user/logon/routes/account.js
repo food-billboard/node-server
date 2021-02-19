a3dd4fc022061c2b5f323d7894d01a3d8ae191c0
gelingfeng <18368003190@163.com>
2021-02-19 09:16:25 +0800

    ci_21

--- src/router/user/logon/routes/account.js
+++ src/router/user/logon/routes/account.js
@@ -58,7 +58,7 @@ router
     ctx.status = 200
     //设置cookie
     //临时设置，需要修改
-    setCookie(ctx, { key: TOKEN_COOKIE, value: token, type: 'set', options: { domain: env.toLowerCase() == 'prod' ? '47.111.229.250' : 'localhost' } })
+    setCookie(ctx, { key: TOKEN_COOKIE, value: token, type: 'set', options: { domain: (env || 'dev').toLowerCase() == 'prod' ? '47.111.229.250' : 'localhost' } })
 
     return {
       fans: fans.length,
